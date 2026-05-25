be068f1aa4a9911788b9671a127726273b760e72
ayuhcl <ayushi-agarwal@hcltech.com>
2026-05-25 12:44:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,4 +5,4 @@ app.get('/', (req, res) => {
   res.send("Hello from ECS Fargate!");
 });
 
-app.listen(3000, () => console.log("Server running"));
+app.listen(3000, "0.0.0.0", () => console.log("Server running"));
